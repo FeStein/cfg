5e0c97216a63a9bbf6d5b4595d62b1d6a6e0bf9e
FelSt <63238040+FeStein@users.noreply.github.com>
2023-11-29 16:05:15 +0100

    Added some configuration files

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -9,10 +9,10 @@ local Caffeine  = require('awake_sleep')
 local Windows = require('window_movements')
 
 -- Timer for Pomodoro
-local Timer = require('timer')
+-- local Timer = require('timer')
 
 -- spoons
-CH = hs.loadSpoon("Cherry")
+-- CH = hs.loadSpoon("Cherry")
 
 -- #############################################################################
 -- Fancy Config Reloading
@@ -30,3 +30,26 @@ function reloadConfig(files)
 end
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
+
+-- #############################################################################
+-- Move Mouse every X minutes
+-- #############################################################################
+--
+function movemouse(x1,y1,x2,y2,sleep)
+local xdiff = x2 - x1
+local ydiff = y2 - y1
+local loop = math.floor( math.sqrt((xdiff*xdiff)+(ydiff*ydiff)) )
+local xinc = xdiff / loop
+local yinc = ydiff / loop
+sleep = math.floor((sleep * 1000000) / loop)
+for i=1,loop do
+x1 = x1 + xinc
+y1 = y1 + yinc
+hs.mouse.absolutePosition({x = math.floor(x1), y = math.floor(y1)})
+hs.timer.usleep(sleep)
+end
+hs.mouse.absolutePosition({x = math.floor(x2), y = math.floor(y2)})
+end
+--movemouse(0,0,1920,1080,2)
+--dwtimer = hs.timer.doEvery(10, function() movemouse(0,0,1920,1080,2) end)
+--dwtimer:start()
